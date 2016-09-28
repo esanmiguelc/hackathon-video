265e1620d76c0142b60b76b50de8764390f894ff
esanmiguelc <esanmiguel@gmail.com>
2016-09-28 15:00:33 -0500

    more stuff

--- mock_server.rb
+++ mock_server.rb
@@ -5,14 +5,17 @@ get '/endpoint' do
   headers 'Access-Control-Allow-Origin' => '*'
   [
     {
+      id: 1,
       title: "First Video",
       description: "First Description",
-      video_url: "http://something.com"
+      video_url: "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4"
     }, {
+      id: 2,
       title: "Second Video",
       description: "Second Description",
       video_url: "http://something.com"
     }, {
+      id: 3,
       title: "Third Video",
       description: "Third Description",
       video_url: "http://something.com"
